2883334a233e41f27f2fcdf2204bdc9a76c5e513
Jaime <56158062+jgarciaodowd@users.noreply.github.com>
2021-01-27 12:53:53 +0100

    Add files via upload

---
+++ ControlAnimaciones.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAnimaciones : MonoBehaviour
+{
+
+    Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool caminar = Input.GetKey(KeyCode.LeftControl);
+        animator.SetBool("Caminar", caminar);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetTrigger("Atacar");
+        }
+    }
+}
